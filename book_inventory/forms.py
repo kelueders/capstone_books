eaffde8d3a30e5cdd333f7c6a1d37172418ff2a7
Kate Lueders <kelueders@gmail.com>
2023-06-27 14:50:59 -0500

    added .env, forms.py, forms.html

---
+++ book_inventory/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class UserLoginForm(FlaskForm):
+    username = StringField('username', validators = [DataRequired()])
+    first_name = StringField('first_name')
+    last_name = StringField('last_name')
+    email = StringField('email', validators = [DataRequired(), Email()])
+    password = PasswordField('password', validators = [DataRequired()])
+    submit_button = SubmitField()
